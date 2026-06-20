bb3410ec967b212437da68f5eb57873260151296
md-alif007 <mohammadalif043@gmail.com>
2026-06-20 12:24:45 +0600

    module 8

--- missions/02_missionTwo/08_moduleEight_backendStruct_userAuth/src/server.ts
+++ missions/02_missionTwo/08_moduleEight_backendStruct_userAuth/src/server.ts
@@ -1,5 +1,5 @@
 import express, { type Application, type Request, type Response } from "express"
-import { Pool } from "pg"
+import { Pool, Result } from "pg"
 
 const app: Application = express()
 const port = 5000
@@ -20,7 +20,7 @@ const initializeDataBase = async () => {
             (
             id SERIAL PRIMARY KEY,
             name VARCHAR(20),
-            email VARCHAR(20) NOT NULL,
+            email VARCHAR(20) UNIQUE NOT NULL,
             password VARCHAR(20) NOT NULL,
             is_active BOOLEAN DEFAULT TRUE ,
             age INT,
@@ -47,18 +47,136 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 // [post method]
-app.post("/", async (req: Request, res: Response) => {
+app.post("/api/users", async (req: Request, res: Response) => {
     // console.log(req.body)
-    const { name, author, email, password } = req.body
-    res.status(201).json({
-        message: "created",
-        data: {
-            name,
-            author,
-            email
-        }
-    });
+    const { name, email, password, age } = req.body
+
+    try {
+        const result = await pool.query(
+            `
+        INSERT INTO 
+        users(name ,email ,password ,age)
+        VALUES($1,$2,$3,$4)
+        RETURNING *
+        `, [name, email, password, age]
+        )
+        console.log(result)
+
+        res.status(201).json({
+            message: "user created successfully! :)",
+            data: result.rows[0]
+        });
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+            error: error
+        });
+    }
 });
+
+
+// [gett method]
+app.get("/api/users", async (req: Request, res: Response) => {
+
+    try {
+        const result = await pool.query(
+            `
+            SELECT * FROM users 
+            `
+        )
+        res.status(200).json({
+            success: true,
+            message: "users retrived successfully! :)",
+            data: result.rows
+        })
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+            error: error
+        })
+    }
+})
+
+// [gett method - single user]
+app.get("/api/users/:id", async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+
+        const result = await pool.query(
+            `
+            SELECT * FROM users WHERE id = $1 
+            `, [id]
+        );
+        // console.log(result)
+
+        if (result.rows.length === 0) {
+            res.status(404).json({
+                success: false,
+                message: " user not found! :( ",
+                data: {}
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "user retrived successfully! :)",
+            data: result.rows[0]
+        })
+
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+            error: error
+        })
+    }
+})
+
+// [putt method]
+app.put("/api/users/:id", async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { name, password, age, is_active } = req.body;
+
+    try {
+
+        const result = await pool.query(
+            `
+        UPDATE users 
+        SET name = $1, 
+        password = $2,
+        age = $3,
+        is_active = $4
+        WHERE id = $5 
+        RETURNING *
+        `, [name, password, age, is_active, id]
+        )
+
+        if (result.rows.length === 0) {
+            res.status(404).json({
+                success: false,
+                message: " user not found! :( ",
+                data: {}
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "user updated successfully! :)",
+            data: result.rows[0]
+        })
+
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+            error: error
+        })
+    }
+
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
