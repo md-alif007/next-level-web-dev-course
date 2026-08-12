8781ba0b4f14865cde370e5404427535c0964e80
md-alif007 <mohammadalif043@gmail.com>
2026-08-12 12:53:09 +0600

    answered question 3 and 4

--- missions/football-ticket-booking-assignment-3/database.sql
+++ missions/football-ticket-booking-assignment-3/database.sql
@@ -160,3 +160,24 @@ from
 where
     full_name ilike 'tanvir%'
     or full_name ilike '%haque%'
+    --3. Retrieve all booking records where the payment status is missing (NULL), 
+    --replacing the empty result with 'Action Required'.
+select
+    booking_id,
+    user_id,
+    match_id,
+    coalesce(payment_status, 'Action Required') as systematic_status
+from
+    bookings
+where
+    payment_status is null
+    --4. Retrieve match booking details along with the User's full name and the scheduled Match fixture teams
+select
+    booking_id,
+    full_name,
+    fixture,
+    total_cost
+from
+    bookings
+    inner join users using (user_id)
+    inner join matches using (match_id)
